56838fba61e7510aba6f5059d41319e40bda2dda
Jacob McFaul <mcjakedude@gmail.com>
2019-03-28 15:34:08 -0700

    All 4 tables with a get and get by id

--- helpers/recipe-router.js
+++ helpers/recipe-router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const Recipe = require('./recipe-crud.js');
+const Ingredient = require('./ingredient-crud.js');
 
 const router = express.Router();
 
 ///////////////////RECIPE////////////////////////
 router.get('/', async (req, res) => {
     try {
-      const recipes = await Recipe.findRecipes(); 
+      const recipes = await Recipe.findRecipes() 
       res.status(200).json(recipes);
     } catch (error) {
       res.status(500).json(error);
